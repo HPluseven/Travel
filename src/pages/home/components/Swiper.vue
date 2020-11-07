6d417348724b5d7511917e3efe6e99bd634b27f0
seven <837709594@qq.com>
2020-11-07 13:27:19 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+          <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/1901/4c/4c16fcadeb147bbea3.img.jpg_200x200_23968f8b.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'https://imgs.qunarzz.com/sight/p0/2005/60/60c020dd5e95629ba3.water.jpg_200x200_8979f256.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 100%
+        background: #eee
+        .swiper-img
+            width: 100%
+</style>
